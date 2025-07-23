cef966dae494c480ef3f104dcd1b863351a5fd7d
ksachin2004 <sskhandekar2026@gmail.com>
2025-07-23 12:20:43 +0530

    frontend url updated

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.use(limiter);
 // CORS: only allow frontend
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://tech-prep-buddy-3fvo.vercel.app"
+  "https://tech-prep-buddy.vercel.app/"
 ];
 
 app.use(cors({
